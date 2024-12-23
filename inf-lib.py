7b4d6a798f07e5668b5532ff7c5d46a636a767ee
Mihael-Kolonistov <gbnjy19738246@gmail.com>
2024-12-23 16:50:11 +0300

    Update inf-lib.py

--- inf-lib.py
+++ inf-lib.py
@@ -3,14 +3,12 @@ import time
 import random
 for i in range(40):
     root = Tk()
-    root.title("Я списал!")
-    def center_window():        
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()       
-        x = random.randint(0, 1000)
-        y = random.randint(0, 1000)
-        root.geometry('%dx%d+%d+%d' % (random.randint(100,500), random.randint(100,500), x, y))    
-    center_window()  
+    root.title("Я списал!")          
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()       
+    x = random.randint(0, 1000)
+    y = random.randint(0, 1000)
+    root.geometry('%dx%d+%d+%d' % (random.randint(100,500), random.randint(100,500), x, y))  
     l=Label(root, text="Я списал у Колонистова")
     l.grid()
     time.sleep(1)
