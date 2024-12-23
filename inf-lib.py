7da678a5812d03a40159426574719bdfbc03d445
Mihael-Kolonistov <gbnjy19738246@gmail.com>
2024-12-23 16:47:10 +0300

    Update inf-lib.py

--- inf-lib.py
+++ inf-lib.py
@@ -1,4 +1,18 @@
 from tkinter import *
-root = Tk()
-root.title("Я списал!")
-root.mainloop()
+import time
+import random
+for i in range(40):
+    root = Tk()
+    root.title("Я списал!")
+    def center_window():        
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()       
+        x = random.randint(0, 1000)
+        y = random.randint(0, 1000)
+        root.geometry('%dx%d+%d+%d' % (random.randint(100,500), random.randint(100,500), x, y))    
+    center_window()  
+    l=Label(root, text="Я списал у Колонистова")
+    root.mainloop()
+    time.sleep(0.1)
+    root.destroy()
+    
